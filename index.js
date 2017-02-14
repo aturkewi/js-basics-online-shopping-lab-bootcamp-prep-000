8d209b1d7977caec32404baa8298e3f6f84c1606
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-02-14 16:03:27 -0500

    Place order

--- index.js
+++ index.js
@@ -15,3 +15,28 @@ function total() {
 
   return t
 }
+
+function placeOrder( cardNumber ) {
+   //var indexToRemove = null;
+   var totalPrice = 0;
+   var currentItem;
+
+
+    for( var i = 0; i < cart.length; i++ ) {
+        // gets first emement in cart and puts in currentItem
+        currentItem = cart[i].price;
+        // adds totalPrice = totalPrice + currentItem (to add the prices together)
+        totalPrice += currentItem;
+        if( cart.length !== null) {
+          cart.splice(cart[i], 1);
+          return cart;
+        } else {
+          if(cardNumber) {
+            console.log(`Your total cost is ${totalPrice}, which will be charged to the card ${cardNumber}.`);
+          } else {
+          console.log('We don\'t have a credit card on file for you to place your order.');
+        }
+        }
+
+    }
+}
